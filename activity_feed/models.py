c77126bdf937b14d233a81a2c4132028ea5ebe3f
saumishr <saumishr@ubuntu.(none)>
2013-06-04 11:21:58 +0530

    Changes to support commenting on albums +  generate proper feed for related actions

--- activity_feed/models.py
+++ activity_feed/models.py
@@ -3,6 +3,7 @@ from django.contrib.comments.signals import comment_was_posted
 
 from mezzanine.blog.models import BlogPost
 from mezzanine.generic.models import ThreadedComment
+from imagestore.models import Album
 
 from actstream import action
 
@@ -13,5 +14,8 @@ def comment_action(sender, comment=None, target=None, **kwargs):
             	target=comment.content_object)
         elif isinstance(comment.content_object, ThreadedComment):
         	action.send(comment.user, verb=u'has commented on review', action_object=comment, 
-            	target=comment.content_object)        
+            	target=comment.content_object)
+        elif isinstance(comment.content_object, Album):
+        	action.send(comment.user, verb=u'has commented on the album', action_object=comment, 
+            	target=comment.content_object)         
 comment_was_posted.connect(comment_action)
