e21a729da0a5bc94a2310c4211f59623ba8c761a
saumishr <saumishr@www.domain1.com>
2013-08-08 02:12:03 +0530

    JS reorg for album/iamges+bug fixes for album

--- userProfile/static/js/album.handlers.js
+++ userProfile/static/js/album.handlers.js
@@ -3,6 +3,7 @@ var delete_album_handler = function(event) {
     var link = add_link.attr('href');
     $.get(link, {}, function(data) {
         $('#vendorAlbum').html(data);
+        install_album_handlers();
     });
     event.stopPropagation();
     event.preventDefault();
@@ -15,6 +16,7 @@ var back_to_album_handler = function(event) {
         var link = add_link.attr('href');
         $.get(link, {}, function(data) {
             $('#vendorAlbum').html(data);
+            install_album_handlers();
         });
         event.stopPropagation();
 		event.preventDefault();
@@ -26,9 +28,7 @@ var show_album_handler = function(event) {
         var link = add_link.attr('href');
         $.get(link, {}, function(data) {
             $('#vendorAlbum').html(data);
-            install_voting_handlers();  //Assumed inheritance from action.handler.js
-			$('.delete_album').off('click').on('click', delete_album_handler);
-			$('.back_to_album').off('click').on('click', back_to_album_handler);
+            install_album_handlers();
 			$('a.thumb').each(function(){
         		$(this).lightBox();
     		});
@@ -38,9 +38,33 @@ var show_album_handler = function(event) {
         return false;
 };
 
+var upload_album_handler = function(event) {
+    var add_link = $(this);
+    var link = add_link.attr('href');
+    $.get(link, {}, function(data) {
+        $('#uploadOrCreate').html(data);
+    });
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+};
+
+var create_album_handler = function(event) {
+    var add_link = $(this);
+    var link = add_link.attr('href');
+    $.get(link, {}, function(data) {
+        $('#uploadOrCreate').html(data);
+    });
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+};
+
 var install_album_handlers = function(){
 	install_voting_handlers();  //Assumed inheritance from action.handler.js
 	$('.delete_album').off('click').on('click', delete_album_handler);
 	$('.back_to_album').off('click').on('click', back_to_album_handler);
 	$('.album_photos').off('click').on('click', show_album_handler);
+    $('.upload_album').off('click').on('click', upload_album_handler);
+    $('.create_album').off('click').on('click', create_album_handler);
 };
