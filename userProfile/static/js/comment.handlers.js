c2b0106e82a4ecb72642b80744ab18ca5b2973f4
saumishr <saumishr@www.domain1.com>
2013-08-09 11:16:18 +0530

    implementation of comment with ajax.

--- userProfile/static/js/comment.handlers.js
+++ userProfile/static/js/comment.handlers.js
@@ -19,3 +19,34 @@ var install_toggle_comment_handler = function() {
     $('.toggle-comment').off('click').on('click', toggle_comment_handler);
 }
 
+var comment_on_object_handler = function(event){
+        var form = $(this);
+        $.ajax({
+            type: form.attr('method'),
+            url: form.attr('action'),
+            data: form.serialize(),
+            success: function (data) {
+                var subcomments_element = form.parents('.subcomments_container').find(".subcomments");
+                form.trigger('reset');
+                subcomments_element.append(data.html);
+                subcomments_element.find('.upvote').off("click").on("click", voting_handlers);
+                subcomments_element.find('.downvote').off("click").on("click", voting_handlers);
+                subcomments_element.find('.clearvote').off("click").on("click", voting_handlers);
+                subcomments_element.find('.pScore').off("click").on("click", display_popup_handler);
+                subcomments_element.find('.broadcasters').off("click").on("click", display_popup_handler);
+            },
+            error: function(data) {
+                alert(data);
+            }
+        });
+
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+};
+
+var install_comment_on_object_handler = function() {
+    $('.comment_on_object').off('submit').on('submit', comment_on_object_handler);
+};
+
+
