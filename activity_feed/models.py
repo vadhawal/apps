0ffac13c80f2ecf07b19c1a7d0087e78b2e51fb7
saumishr <saumishr@www.domain1.com>
2013-08-29 02:09:16 +0530

    Moved action verbs as settings.py constants

--- activity_feed/models.py
+++ activity_feed/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.comments.signals import comment_was_posted
 from django.contrib.auth.models import User
+from django.conf import settings
+
 from mezzanine.blog.models import BlogPost
 from mezzanine.generic.models import ThreadedComment
 from imagestore.models import Album, Image
@@ -12,27 +14,27 @@ from follow.models import Follow
 def comment_action(sender, comment=None, target=None, **kwargs):
     if comment.user:
         if isinstance(comment.content_object, BlogPost):
-            action.send(comment.user, verb=u'has posted a review on', action_object=comment, 
-                target=comment.content_object)
+            action.send(comment.user, verb=settings.REVIEW_POST_VERB, action_object=comment.content_object, 
+                target=comment)
             Follow.objects.get_or_create(comment.user, comment)
             actions.follow(comment.user, comment, send_action=False, actor_only=False) 
         elif isinstance(comment.content_object, ThreadedComment):
-        	action.send(comment.user, verb=u'has commented on review', action_object=comment, 
+        	action.send(comment.user, verb=settings.REVIEW_COMMENT_VERB, action_object=comment, 
             	target=comment.content_object, batch_time_minutes=30, is_batchable=True)
         elif isinstance(comment.content_object, Album):
-        	action.send(comment.user, verb=u'has commented on the album', action_object=comment, 
+        	action.send(comment.user, verb=settings.ALBUM_COMMENT_VERB, action_object=comment, 
             	target=comment.content_object, batch_time_minutes=30, is_batchable=True)
         elif isinstance(comment.content_object, Image):
-            action.send(comment.user, verb=u'has commented on the image', action_object=comment, 
+            action.send(comment.user, verb=settings.IMAGE_COMMENT_VERB, action_object=comment, 
                 target=comment.content_object, batch_time_minutes=30, is_batchable=True)
         elif isinstance(comment.content_object, UserWishRadio):
             obj = comment.content_object
             owner = obj.content_type.get_object_for_this_type(pk=obj.object_id)
             if isinstance(owner, BlogPost):
-                action.send(comment.user, verb=u'has commented on the deal', action_object=comment, 
+                action.send(comment.user, verb=settings.DEAL_COMMENT_VERB, action_object=comment, 
                     target=comment.content_object, batch_time_minutes=30, is_batchable=True)
             elif isinstance(owner, User):
-                action.send(comment.user, verb=u'has commented on the wish', action_object=comment, 
+                action.send(comment.user, verb=settings.WISH_COMMENT_VERB, action_object=comment, 
                     target=comment.content_object, batch_time_minutes=30, is_batchable=True)
             else:
                 """
