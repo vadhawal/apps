9fb0ead53376ccb7f211129d03d42b37755b4cbb
saumishr <saumishr@www.domain1.com>
2013-09-07 19:15:57 +0530

    login required handler implementation

--- userProfile/static/js/voting.handlers.js
+++ userProfile/static/js/voting.handlers.js
@@ -1,22 +1,25 @@
 var voting_handlers = function(event){
-		var add_link = $(this);
-		$.post(add_link.attr('href'),{HTTP_X_REQUESTED:'XMLHttpRequest'},
+	    if(login_required_handler())
+        	return false; 
+		var $elementClicked = $(this);
+		var url =  $elementClicked.data("href");
+		$.post(url, {HTTP_X_REQUESTED:'XMLHttpRequest'},
 	       function(data) {
 	           if (data.success == true) {
-	               add_link.parent().find('a.pScore').text(data.score.num_up_votes);
-	               if(add_link.hasClass('upvote'))
+	               $elementClicked.parent().find('a.pScore').text(data.score.num_up_votes);
+	               if($elementClicked.hasClass('upvote'))
 	               {
-	               		add_link.removeClass('upvote').addClass('clearvote');
-	               		add_link.text('Unlike');
-	               		$newhref = add_link.attr('href').replace('up', 'clear');
-	               		add_link.attr('href', $newhref);
+	               		$elementClicked.removeClass('upvote').addClass('clearvote');
+	               		$elementClicked.text('Unlike');
+	               		$newhref = url.replace('up', 'clear');
+	               		$elementClicked.data('href', $newhref);
 	               }
-	               else if(add_link.hasClass('clearvote'))
+	               else if($elementClicked.hasClass('clearvote'))
 	               {
-	               		add_link.removeClass('clearvote').addClass('upvote');
-	               		add_link.text('Like');
-	               		$newhref = add_link.attr('href').replace('clear', 'up');
-	               		add_link.attr('href', $newhref);
+	               		$elementClicked.removeClass('clearvote').addClass('upvote');
+	               		$elementClicked.text('Like');
+	               		$newhref = url.replace('clear', 'up');
+	               		$elementClicked.data('href', $newhref);
 	               }
 	           } else {
 	               alert('ERROR: ' + data.error_message);
@@ -28,33 +31,33 @@ var voting_handlers = function(event){
 };
 
 var review_voting_handler = function(event){
-		var add_link = $(this);
-		$.post(add_link.attr('href'),{HTTP_X_REQUESTED:'XMLHttpRequest'},
+		if(login_required_handler())
+        	return false;
+		var $elementClicked = $(this);
+		var url =  $elementClicked.data("href");
+		$.post(url, {HTTP_X_REQUESTED:'XMLHttpRequest'},
 	       function(data) {
 	           if (data.success == true) {
-	               add_link.parent().find('a.pScore').text(data.score.num_up_votes);
-	               if(add_link.hasClass('found-helpful'))
+	               $elementClicked.parent().find('a.pScore').text(data.score.num_up_votes);
+	               if($elementClicked.hasClass('found-helpful'))
 	               {
-	               		var url = add_link.attr('href');
 	               		var new_url = url.replace('up', 'clear');
 	               		var html = '</span>You found this review helpful.</span><a class="clear-helpful" href="'+new_url+'">Clear</a>';
-	               		add_link.parent().html(html);
+	               		$elementClicked.parent().html(html);
 	               }
-	               else if(add_link.hasClass('not-found-helpful'))
+	               else if($elementClicked.hasClass('not-found-helpful'))
 	               {
-	               		var url = add_link.attr('href');
 	               		var new_url = url.replace('down', 'clear');
 	               		var html = '</span>You did not find this review helpful!</span><a class="clear-helpful" href="'+new_url+'">Clear</a>';
-	               		add_link.parent().html(html);
+	               		$elementClicked.parent().html(html);
 	               }
-	               else if(add_link.hasClass('clear-helpful'))
+	               else if($elementClicked.hasClass('clear-helpful'))
 	               {
-	               		var url = add_link.attr('href');
 	               		var up_url = url.replace('clear', 'up');
 	               		var down_url = url.replace('clear', 'down');
 	               		var html = '<span>Do you find this review helpful? </span><a class="found-helpful" href="'+up_url+'">Yes </a>'+
                         '<a class="not-found-helpful" href="'+down_url+'">No</a>';
-                        add_link.parent().html(html);
+                        $elementClicked.parent().html(html);
 	               }
 	               install_voting_handlers();
 	           } else {
--- userProfile/static/js/wishradiobase.js
+++ userProfile/static/js/wishradiobase.js
@@ -1,5 +1,15 @@
-var vendor_followers_handler = function(event) {
+var login_required_handler = function()
+{
+    if(!is_authenticated)
+    {
+        var goto_url = login_url +'?next=' + window.location.pathname;
+        document.location.href = goto_url; 
+        return true;
+    }
+    return false;
+}
 
+var vendor_followers_handler = function(event) {
     event.preventDefault();
     $("<div id='pop_up'><span class='button b-close'><span>X</span></span></div>").appendTo("body").addClass('popup');
     $('#pop_up').bPopup({
@@ -14,7 +24,7 @@ var vendor_followers_handler = function(event) {
     });
 };
 
-var FollowUnfollow = function($elementClicked, added) {
+var FollowUnfollow = function($elementClicked, new_count) {
     var $element = $elementClicked.parent();
     var classes = $element.attr('class');
     if (classes.indexOf('following') == -1) {
@@ -26,21 +36,32 @@ var FollowUnfollow = function($elementClicked, added) {
     $element.attr('class', classes); 
     var $followerCountElement = $("#" + $elementClicked.data("id"));
     if ($followerCountElement) {
-        var followerCount = $followerCountElement.html();
-        followerCount = added ? parseInt(followerCount) + 1 : parseInt(followerCount) - 1;
-        var newHTML = followerCount;
+        console.log($followerCountElement.html());
+        var newHTML = new_count;
         $followerCountElement.html(newHTML);
     }
 }
 
 var follow_handler = function(event) {
-    var $add_link = $(this);
-    FollowUnfollow($add_link, true);
-    $.post($add_link.attr('href'), {}, function(data) {
-        if (data  == 'ok') {
+    if(login_required_handler())
+        return false;
+
+    var $elementClicked = $(this);
+    $elementClicked.off("click", follow_handler);
+    var $followerCountElement = $("#" + $elementClicked.data("id"));
+    var followerCount = $followerCountElement.html();
+    followerCount = parseInt(followerCount) + 1;
+    $followerCountElement.html(followerCount);
+    var url =  $elementClicked.data("href");
+
+    $.post(url, {}, function(data) {
+        var return_val = JSON.parse(data)
+        if (return_val.success  == true) {
+            FollowUnfollow($elementClicked, return_val.count);
         } else {
             //showSnazzySuccessMessage("Something went wrong!");
         }
+        $elementClicked.on("click", follow_handler);
     });
     event.stopPropagation();
     event.preventDefault();
@@ -48,13 +69,25 @@ var follow_handler = function(event) {
 };
 
 var unfollow_handler = function(event) {
-        var $add_link = $(this);
-        FollowUnfollow($add_link, false);
-        $.post($add_link.attr('href'), {}, function(data) {
-            if (data == 'ok') {
-            } else {
-                //showSnazzySuccessMessage("Something went wrong!");
-            }
+    if(login_required_handler())
+        return false;
+
+    var $elementClicked = $(this);
+    $elementClicked.off("click", unfollow_handler);
+    var url =  $elementClicked.data("href");
+    var $followerCountElement = $("#" + $elementClicked.data("id"));
+    var followerCount = $followerCountElement.html();
+    followerCount = parseInt(followerCount) - 1;
+    $followerCountElement.html(followerCount);
+
+    $.post(url, {}, function(data) {
+        var return_val = JSON.parse(data)
+        if (return_val.success  == true) {
+            FollowUnfollow($elementClicked, return_val.count);
+        } else {
+            //showSnazzySuccessMessage("Something went wrong!");
+        }
+        $elementClicked.on("click", unfollow_handler);
     });
     event.stopPropagation();
     event.preventDefault();
@@ -67,6 +100,8 @@ var install_follow_handlers = function() {
 }
 
 var sharewish_handler = function() {
+    if(login_required_handler())
+        return false;
     var add_link = $(this);
     var link = add_link.attr('href');
     $.get(link, {}, function(data) {
