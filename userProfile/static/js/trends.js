2bc1f3a740c8f18954d774cd9cff40cbc98766e9
saumishr <saumishr@www.domain1.com>
2013-09-30 01:12:50 +0530

    beautification of scrllbars + added scrollbars in category box

--- userProfile/static/js/trends.js
+++ userProfile/static/js/trends.js
@@ -31,15 +31,18 @@
 var get_trending_deals_handler = function(parent_category, sub_category)
 {
     $.get('/' + parent_category + '/'+ sub_category + '/trendingdeals', {}, function(data) {
-            $('#topDealsForStoreCategory').html(data);
+          $('#topDealsForStoreCategory').html(data);
             install_voting_handlers();
             $('.shareWish').on('click', sharewish_handler);
 
             $("#topDealsForStoreCategory").imagesLoaded({
                 complete: function(images) {
                     $(".dealBox_h").mCustomScrollbar({
-                    horizontalScroll:true,
-                    theme:"dark-thick"
+                      horizontalScroll:true,
+                      theme:"dark-thick",
+                      mouseWheel:true,
+                      autoHideScrollbar:true,
+                      contentTouchScroll:true
                   });
                 }
             });
