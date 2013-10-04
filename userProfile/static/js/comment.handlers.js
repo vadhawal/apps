63c55211cf84f11878095615cb9ff65f23269e3a
saumishr <saumishr@www.domain1.com>
2013-10-05 01:24:25 +0530

    ajax comment submission in actstream'

--- userProfile/static/js/comment.handlers.js
+++ userProfile/static/js/comment.handlers.js
@@ -22,14 +22,14 @@ var install_toggle_comment_handler = function() {
 var comment_on_object_handler = function(event){
 	    if(login_required_handler())
         	return false;
-        var form = $(this);
+        var $form = $(this);
         $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize(),
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
             success: function (data) {
-                var subcomments_element = form.parents('.subcomments_container').find(".subcomments");
-                form.trigger('reset');
+                var subcomments_element = $form.parents('.subcomments_container').find(".subcomments");
+                $form.trigger('reset');
                 subcomments_element.append(data.html);
                 subcomments_element.find('.upvote').off("click").on("click", voting_handlers);
                 subcomments_element.find('.downvote').off("click").on("click", voting_handlers);
@@ -37,7 +37,7 @@ var comment_on_object_handler = function(event){
                 subcomments_element.find('.pScore').off("click").on("click", display_popup_handler);
                 subcomments_element.find('.broadcasters').off("click").on("click", display_popup_handler);
                 install_toggle_comment_handler();
-                var $formParent = form.parents(".subcomments_container");
+                var $formParent = $form.parents(".subcomments_container");
                 var $total_comments = $formParent.find('.total_comments');
                 var $loaded_comments = $formParent.find('.loaded_comments');
                 var total_comments = parseInt($total_comments.text());
@@ -46,6 +46,7 @@ var comment_on_object_handler = function(event){
                 loaded_comments = loaded_comments + 1;
                 $total_comments.text(total_comments);
                 $loaded_comments.text(loaded_comments);
+                $form.find('.subcomment_text').trigger('autosize.resize');
             },
             error: function(data) {
                 console.log(data);
@@ -91,14 +92,36 @@ var view_previous_comments_handler = function(event){
 };
 
 var comment_radio_handler = function(event){
-    $(this).parent().find('textarea[name="comment"]').focus();
+	var $text_element = $(this).parents('.radio_module').parent().find('.subcomment_text');
+	if($text_element)
+		$text_element.focus();
+
     event.stopPropagation();
     event.preventDefault();
     return false;
 };
 
+var comment_on_object_key_handler = function(event){
+	if(event.which == 13) {
+		var $text_element = $(this);
+		$text_element.blur();
+		var $form = $text_element.parents('.comment_on_object');
+		if($form) {
+			$form.find('.subcomment_text').trigger('autosize.resize');
+			$form.submit();
+		}
+		
+		event.stopPropagation();
+    	event.preventDefault();
+		return false;	
+	}
+
+}
+
 var install_comment_on_object_handler = function() {
     $('.comment_on_object').off('submit').on('submit', comment_on_object_handler);
+    $('.subcomment_text').off('keyup').on('keyup', comment_on_object_key_handler);
+    $('.subcomment_text').autosize({append: "\n"});
     $('.viewPreviousComments').off('click').on('click', view_previous_comments_handler);
     $('.comment_radio').off('click').on('click', comment_radio_handler);
 };
