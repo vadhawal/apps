5eaa532d38bde813d5d8d8cd25829a6787c70e0d
saumishr <saumishr@www.domain1.com>
2013-10-23 04:16:37 +0530

    JS changes for version 1 implementation of album/image create/edit/delete workflow

--- userProfile/static/js/album.handlers.js
+++ userProfile/static/js/album.handlers.js
@@ -56,15 +56,49 @@ var upload_album_handler = function(event) {
 };
 
 var create_album_handler = function(event) {
+    if(login_required_handler())
+        return false;
     var add_link = $(this);
     var link = add_link.attr('href');
-    $.get(link, {}, function(data) {
-        $('#uploadOrCreate').html(data);
+
+    $("<div id='pop_up'><span class='button b-close'><span>X</span></span></div>").appendTo("body").addClass('popup');
+    $('#pop_up').bPopup({
+        content:'ajax',
+        loadUrl:link,
+        zIndex: 2,
+        onClose: function(){ $('#pop_up').remove(); },
+        scrollBar:'true',
+        loadCallback: function(){
+            $('.album-form').on('submit', album_form_submit_handler);
+        }
     });
+
     event.stopPropagation();
     event.preventDefault();
     return false;
 };
+
+var album_form_submit_handler = function() {
+    var $form = $(this);
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        success: function (data) {
+            var ret_data = JSON.parse(data);
+            var url = ret_data.url;
+            window.location.assign(url);
+        },
+        error: function(xhr) {
+            var errors = JSON.parse(xhr.responseText);
+            $('#pop_up').find('.error').removeClass('error');
+            $.each( errors, function( key, value ) {
+                $('#pop_up').find('[name="' + key + '"]').addClass('error');
+            });
+        }
+    });
+    return false;
+}
 
 var install_album_handlers = function($parent_element){
 	install_voting_handlers($parent_element);  //Assumed inheritance from action.handler.js
