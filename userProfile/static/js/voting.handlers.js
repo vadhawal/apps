401c9fd14a2e73ad37f62bd683828f4d30ea860c
vadhawal <labuser@vickMacPro.local>
2013-10-26 20:40:43 +0530

    incorporating gayatris feedback

--- userProfile/static/js/voting.handlers.js
+++ userProfile/static/js/voting.handlers.js
@@ -1,6 +1,8 @@
 var VOTING_STRING = {
 	foundReviewHelpful: 'You found this review helpful.',
-	foundReviewHelpfulQues: 'Was this review helpful?'
+	foundReviewHelpfulQues: 'Was this review helpful?',
+	like: 'Like',
+	yes: 'Yes'
 };
 
 var display_popup_handler = function(event) {
@@ -39,18 +41,22 @@ var voting_handlers = function(event){
 		$.post(url, {HTTP_X_REQUESTED:'XMLHttpRequest'},
 	       function(data) {
 	           if (data.success == true) {
-	               $elementClicked.parent().find('a.pScore').text('('+data.score.num_up_votes+')');
+	               $elementClicked.parent().parent().find('a.pScore').text('('+data.score.num_up_votes+')');
 	               if($elementClicked.hasClass('upvote'))
 	               {
 	               		$elementClicked.removeClass('upvote').addClass('clearvote');
-	               		$elementClicked.text('Unlike');
+	               		$elementClicked.parent().children().first().text(VOTING_STRING.like);
+	               		$elementClicked.parent().children().first().addClass('clearvote');
+	               		$elementClicked.children().first().text("");
 	               		$newhref = url.replace('up', 'clear');
 	               		$elementClicked.data('href', $newhref);
 	               }
 	               else if($elementClicked.hasClass('clearvote'))
 	               {
 	               		$elementClicked.removeClass('clearvote').addClass('upvote');
-	               		$elementClicked.text('Like');
+	               		$elementClicked.children().first().text(VOTING_STRING.like);
+	               		$elementClicked.parent().children().first().text("");
+	               		$elementClicked.parent().children().first().removeClass('clearvote');
 	               		$newhref = url.replace('clear', 'up');
 	               		$elementClicked.data('href', $newhref);
 	               }
@@ -71,30 +77,34 @@ var review_voting_handler = function(event){
 		$.post(url, {HTTP_X_REQUESTED:'XMLHttpRequest'},
 	       function(data) {
 	           if (data.success == true) {
-					var $parent = $elementClicked.parent();
-					var removeClass = "";
-					var addClass = "";
-					var new_url = "";
-					var spanText = "";
+					var $parent = $elementClicked.parent(),
+						removeClass = "",
+						addClass = "",
+						new_url = "",
+						spanText = "",
+						innerSpantText = "";
 					$parent.find('a.pScore').text('(' + data.score.num_up_votes + ')');
 					if($elementClicked.hasClass('found-helpful'))
 					{
 						new_url = url.replace('up', 'clear');
 						spanText = VOTING_STRING.foundReviewHelpful;
-						removeClass = 'found-helpful';
+						removeClass = 'found-helpful radioColorBackground';
 						addClass = 'clear-helpful';
+						innerSpantText = "";
 					}
 					else if($elementClicked.hasClass('clear-helpful'))
 					{
-						addClass = 'found-helpful';
+						addClass = 'found-helpful radioColorBackground';
 						removeClass = 'clear-helpful';
 					 	new_url = url.replace('clear', 'up');
 						spanText = VOTING_STRING.foundReviewHelpfulQues;
+						innerSpantText = VOTING_STRING.yes;
 					}
 					$elementClicked.removeClass(removeClass);
 					$elementClicked.addClass(addClass);
+					$elementClicked.children().first().text(innerSpantText);
 					$elementClicked.data("href", new_url);
-					$parent.find('span').text(spanText);
+					$parent.children('span').text(spanText);
 					install_review_voting_handler($parent);
 	           } else {
 	               alert('ERROR: ' + data.error_message);
@@ -107,16 +117,16 @@ var review_voting_handler = function(event){
 var install_voting_handlers = function($parent_element){
 	if($parent_element)
 	{
-		$parent_element.find('.upvote').off("click", voting_handlers).on("click", voting_handlers);
-		$parent_element.find('.downvote').off("click", voting_handlers).on("click", voting_handlers);
-		$parent_element.find('.clearvote').off("click", voting_handlers).on("click", voting_handlers);
-		$parent_element.find('.pScore').off("click", display_popup_handler).on("click", display_popup_handler);
-		$parent_element.find('.broadcasters').off("click", display_popup_handler).on("click", display_popup_handler);
+		$parent_element.find('a.upvote').off("click", voting_handlers).on("click", voting_handlers);
+		$parent_element.find('a.downvote').off("click", voting_handlers).on("click", voting_handlers);
+		$parent_element.find('a.clearvote').off("click", voting_handlers).on("click", voting_handlers);
+		$parent_element.find('a.pScore').off("click", display_popup_handler).on("click", display_popup_handler);
+		$parent_element.find('a.broadcasters').off("click", display_popup_handler).on("click", display_popup_handler);
 	}
 	else
 	{
-    	$('.upvote').add('.downvote').add('.clearvote').off("click", voting_handlers).on("click", voting_handlers);
-    	$('.pScore').add('.broadcasters').off("click", display_popup_handler).on("click", display_popup_handler);
+    	$('a.upvote').add('a.downvote').add('a.clearvote').off("click", voting_handlers).on("click", voting_handlers);
+    	$('a.pScore').add('a.broadcasters').off("click", display_popup_handler).on("click", display_popup_handler);
     }
 };
 var install_review_voting_handler = function($parent_element) {
