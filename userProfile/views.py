bebf2de9f3e881743fec09c54c4fd3429df60c09
saumishr <saumishr@www.domain1.com>
2014-01-30 18:38:45 +0530

    Add textarea for email_message on signup

--- userProfile/views.py
+++ userProfile/views.py
@@ -1194,6 +1194,7 @@ def contact_us(request, template="generic/contact_us.html"):
 				return HttpResponse(data, **response_kwargs)
 		else:
 			form = ContactUsForm()
+			form.fields['email_message'].widget.attrs['style'] = 'resize:none;'
 			if request.user.is_authenticated():
 				form.fields['email_from'].initial = request.user.email
 
